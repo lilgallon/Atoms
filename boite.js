434fa72da74720491cd0b986ed0afd42219f39e1
Lilian Gallon <lilian.gallon@etu.u-bordeaux.fr>
2018-02-27 18:43:13 +0100

    Ajout univers / placement atomes & fix random float -> int

--- boite.js
+++ boite.js
@@ -10,7 +10,7 @@ function creationTableau(){
         table.appendChild(tr);
         for(var j = 0; j<size; ++j){
             var td = d.createElement("td");
-            var cell = new cellule(i,j);
+            var cell = univers[i][j];//new cellule(i,j);
             cell.dom = td;
             cell.initialiser();
             td.style.width = "25px";
@@ -38,6 +38,9 @@ function cellule(li, co){
             couleur = "grey";
         } else if (li === 0 || co === 0 || co === size - 1 || li === size - 1) {
             couleur = "darkgrey";
+            this.dom.addEventListener("click", function(){
+                gererAppuiGris(instance);
+            });
         } else {
             this.dom.addEventListener("click", function(){
                 gererAppuiBlanc(instance);
@@ -64,7 +67,53 @@ function gererAppuiBlanc(cell){
     }
 }
 
+function gererAppuiGris(cell){
+
+    if(cell.dom.style.getPropertyValue("background-color") !== "darkgrey"){ // trop bien là il faut un tiret
+        cell.dom.style.backgroundColor = "darkgrey";
+    }else{
+        var red = Math.floor(Math.random() * 255);
+        var green = Math.floor(Math.random() * 255);
+        var blue = Math.floor(Math.random() * 255);
+
+        cell.dom.style.backgroundColor = "rgb(" + red + ","  +green + "," + blue + ")";
+    }
+}
+
+function initAtomes(){
+    var nb_atomes = 5;
+    var lignes_disponibles = [];
+    var colonnes_disponibles = [];
+
+    for(var i = 1 ; i < size-1 ; ++i){
+        lignes_disponibles.push(i);
+        colonnes_disponibles.push(i);
+    }
+
+    for(var i = 1 ; i <= nb_atomes ; ++i) {
+        var li = lignes_disponibles[Math.floor(Math.random() * lignes_disponibles.length)];
+        var co = colonnes_disponibles[Math.floor(Math.random() * colonnes_disponibles.length)];
+        univers[li][co].atome = true;
+        lignes_disponibles.slice(li, 1);
+        colonnes_disponibles.slice(co, 1);
+    }
+}
+
+function creerUnivers(){
+    var li, co, ligne;
+    univers = [];
+    for(li = 0; li < size ; li += 1){
+        ligne = [];
+        for(co = 0; co < size; co += 1){
+            ligne.push(new cellule(li,co));
+        }
+        univers.push(ligne);
+    }
+}
+
 
 function init() {
+    creerUnivers();
+    initAtomes();
     creationTableau();
 }
